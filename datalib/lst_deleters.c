3b3b7ff02d74f9479cecf3f43e78ed7ef1ae91ed
Francisco Javier Gutierrez Perez <frgutier@c3r3s4.42malaga.com>
2023-01-29 13:44:47 +0100

    Creadas las funciones de eliminar listas enlazadas
    He creado las distintas funciones que se encargan de eliminar listas
    enlazadas.

--- datalib/lst_deleters.c
+++ datalib/lst_deleters.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   lst_deleters.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: antgalan <antgalan@student.42malaga.com    +#+  +:+       +#+        */
+/*   By: frgutier <frgutier@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/02 00:58:54 by antgalan          #+#    #+#             */
-/*   Updated: 2023/01/23 11:52:14 by antgalan         ###   ########.fr       */
+/*   Updated: 2023/01/29 13:38:33 by frgutier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,85 @@
 
 void	lst_remove_first(t_list **list)
 {
-	// TODO
+	t_list	*tmp;
+
+	if (*list)
+	{
+		tmp = (*list)->next;
+		free(*list);
+		*list = tmp;
+	}
 }
 
 void	lst_remove(t_list **list, t_list *elem)
 {
-	// TODO
+	t_list	*tmp;
+
+	if (*list)
+	{
+		if (*list == elem)
+			lst_remove_first(list);
+		else
+		{
+			tmp = *list;
+			while (tmp->next != elem)
+				tmp = tmp->next;
+			tmp->next = elem->next;
+			free(elem);
+		}
+	}
 }
 
 void	lst_remove_last(t_list **list)
 {
-	// TODO
+	t_list	*tmp;
+
+	if (*list)
+	{
+		if ((*list)->next == NULL)
+			lst_remove_first(list);
+		else
+		{
+			tmp = *list;
+			while (tmp->next->next != NULL)
+				tmp = tmp->next;
+			free(tmp->next);
+			tmp->next = NULL;
+		}
+	}
 }
 
 int	lst_purge(t_list **list, void *data)
 {
-	// TODO
+	t_list	*tmp;
+
+	if (*list)
+	{
+		if ((*list)->data == data)
+			lst_remove_first(list);
+		else
+		{
+			tmp = *list;
+			while (tmp->next->data != data)
+				tmp = tmp->next;
+			tmp->next = tmp->next->next;
+			free(tmp->next);
+		}
+	}
+	return (0);
 }
 
 void	lst_clear(t_list **list)
 {
-	// TODO
+	t_list	*tmp;
+
+	if (*list)
+	{
+		while (*list)
+		{
+			tmp = (*list)->next;
+			free(*list);
+			*list = tmp;
+		}
+	}
 }
